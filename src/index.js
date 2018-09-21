3d5809a1d72812a62eb157d1497c324322afa7c2
pomber <pombopombopombo@gmail.com>
2018-09-21 15:53:32 -0400

    Add loading screen

--- src/index.js
+++ src/index.js
@@ -1,6 +1,26 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
-import App from "./app";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render(
+  <div
+    style={{
+      height: "100%",
+      display: "flex",
+      justifyContent: "center",
+      alignItems: "center"
+    }}
+  >
+    <p>Loading...</p>
+  </div>,
+  document.getElementById("root")
+);
+
+const pathname = window.location.pathname;
+
+if (pathname !== "/publish") {
+  import("./app").then(m => {
+    const App = m.default;
+    ReactDOM.render(<App />, document.getElementById("root"));
+  });
+}
