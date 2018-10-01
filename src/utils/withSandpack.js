0d48cf96189df72a9e402a750e54a55b007c2526
pomber <pombopombopombo@gmail.com>
2018-10-01 00:04:48 -0300

    Remove dead code

--- src/utils/withSandpack.js
+++ src/utils/withSandpack.js
@@ -20,26 +20,3 @@ export default function withSandpack(Component) {
     <WrappedComponent {...props} forwardedRef={ref} />
   ));
 }
-
-// function logProps(Component) {
-//   class LogProps extends React.Component {
-//     componentDidUpdate(prevProps) {
-//       console.log("old props:", prevProps);
-//       console.log("new props:", this.props);
-//     }
-
-//     render() {
-//       const { forwardedRef, ...rest } = this.props;
-
-//       // Assign the custom prop "forwardedRef" as a ref
-//       return <Component ref={forwardedRef} {...rest} />;
-//     }
-//   }
-
-//   // Note the second param "ref" provided by React.forwardRef.
-//   // We can pass it along to LogProps as a regular prop, e.g. "forwardedRef"
-//   // And it can then be attached to the Component.
-//   return React.forwardRef((props, ref) => {
-//     return <LogProps {...props} forwardedRef={ref} />;
-//   });
-// }
